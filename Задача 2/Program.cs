f6ffd3a36bb2c11e3201da26ff84b6cf3fb30f41
Tatiana <tatiana.kaprilova@mail.ru>
2023-09-19 08:03:26 +0300

    Commit1_Задача2

---
+++ Задача 2/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите первое число: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int number2 = Convert.ToInt32(Console.ReadLine());
+if (number1 > number2) 
+{
+Console.WriteLine($"max = {number1}, min = {number2}");
+}
+    
+else
+{
+    Console.WriteLine($"max = {number2}, min = {number1}");
+}
